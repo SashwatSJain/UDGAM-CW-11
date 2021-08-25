f324da73f8911dce695e6d43d71093caeaa51cfd
Sashwat Jain <sashwatsjain@gmail.com>
2021-08-25 11:34:08 +0530

    Update 8.py

--- 25 August 2021/8.py
+++ 25 August 2021/8.py
@@ -4,10 +4,14 @@ prime = []
 for x in range(1, a+1):
     for y in range(2,x):
         if x%y == 0:
-            prime.update(x)
+            notprime.insert(0, x)
             break
         else:
-            notprime.update(x)
+            prime.insert(0, x)
             pass
 
-print(prime, notprime)
+
+notprime = list(dict.fromkeys(notprime))
+print(notprime)
+prime = list(dict.fromkeys(prime))
+print(prime)
