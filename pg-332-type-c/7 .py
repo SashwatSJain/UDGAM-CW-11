0a2559266444c6f0e90068f937e037447335132e
Sashwat Jain <sashwatsjain@gmail.com>
2021-09-06 10:43:49 +0530

    Update 7 .py

---
+++ pg-332-type-c/7 .py
@@ -0,0 +1,7 @@
+dictt = {0:"zero", 1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine"}
+digit = int(input("number(less than nine) : "))
+
+if digit in dictt:
+    print(dictt[digit])
+else:
+    print("invalid digit")
