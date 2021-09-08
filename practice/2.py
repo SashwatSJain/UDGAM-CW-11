f48be6f1e989b708a6fd78f5264991dc4214056d
Sashwat Jain <sashwatsjain@gmail.com>
2021-09-08 08:31:35 +0530

    Update 2.py

--- practice/2.py
+++ practice/2.py
@@ -6,10 +6,23 @@ while True:
     num += 1
     sum += num
     print(num)
-    a = input('>>>')
+    a = input('>>>(y to print sum and terminate)')
     if str(a) != "y":
         pass
     else:
         # the print(sum) line can be put outside the loop also to get the same output
         print(sum)
         break
+
+"""
+output:
+1
+>>>
+2
+>>>
+3
+>>>
+4
+>>>y
+10
+"""
