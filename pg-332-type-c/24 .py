56fbabfeb8023b80510197cbf0332d810f11fc7c
Sashwat Jain <sashwatsjain@gmail.com>
2021-09-19 14:59:24 +0530

    Update 24 .py

---
+++ pg-332-type-c/24 .py
@@ -0,0 +1,45 @@
+# a)
+for x in range(1, 4):
+    n = 3-x
+    print(' '* n + '* ' * x)
+
+for x in range(1, 4):
+    n = 3-x
+    print(' '* x + '* ' * n)
+
+# b)
+for x in range(1, 4):
+    print('* ' * x)
+
+for x in range(1, 4):
+    n = 3-x
+    print('* ' * n)
+
+# c)
+for x in range(1, 4):
+    n = 3-x
+    if x==1:
+        print(' '* n + '*' + ' '*n)
+    elif x==2:
+        print(' '+'*' + ' ' + '*')
+
+    else:
+        print('*' + ' '*x + '*')
+
+for x in range(1, 4):
+    n = 3-x
+    print(' '* x + '* ' * n)
+
+# d)
+for x in range(7):
+    t = x-1
+    print('* ', end='')
+    if x==0:
+        # print('')
+        pass
+    elif x<4:
+        print(' '* t, '*', sep = '', end='')
+    else:
+        r = 6-x
+        print(' '*r,'*',  end = '')
+    print('')
