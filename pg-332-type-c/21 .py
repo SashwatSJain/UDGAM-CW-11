ab8d28c9cb669daf4a614caac33f900b603503cb
Sashwat Jain <sashwatsjain@gmail.com>
2021-09-19 14:37:54 +0530

    Update 21 .py

--- pg-332-type-c/21 .py
+++ pg-332-type-c/21 .py
@@ -1,8 +1,9 @@
 from math import factorial
 x = int(input(""))
-summ = 0
-for i in range(1, 7):
-    n = (x**i)/factorial(i)
+summ = 1.0
+print(summ)
+for i in range(1, x+1):
+    n = 1/factorial(i)
     if i%2 == 0:
         n*=-1
     else:
